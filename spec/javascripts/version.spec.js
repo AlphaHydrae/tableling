c1f5df6328af2828d2c8f80c0704d1c30425582c
AlphaHydrae <hydrae.alpha@gmail.com>
2013-02-21 17:04:42 +0100

    Version bump to 0.0.15.

--- spec/javascripts/version.spec.js
+++ spec/javascripts/version.spec.js
@@ -2,6 +2,6 @@
 describe("version", function() {
 
   it("should be correct", function() {
-    expect(Tableling.version).toBe('0.0.14');
+    expect(Tableling.version).toBe('0.0.15');
   });
 });
--- src/tableling.js
+++ src/tableling.js
@@ -1,7 +1,7 @@
 Backbone.Tableling = Tableling = (function(Backbone, _, $){
 
   var Tableling = {
-    version : "0.0.14"
+    version : "0.0.15"
   };
 
   //= tableling.core.js

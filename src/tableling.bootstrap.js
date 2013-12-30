06c330919721dec1e0a8739dc006735700f03f1e
Simon Oulevay <simon.oulevay@lotaris.com>
2013-12-30 14:36:55 +0100

    Added clearfix to bootstrap table header and footer.

--- src/tableling.bootstrap.js
+++ src/tableling.bootstrap.js
@@ -1,7 +1,7 @@
 Tableling.Bootstrap = {};
 
 Tableling.Bootstrap.Table = Tableling.Plain.Table.extend({
-  template : _.template('<div class="header"><div class="pageSize pull-left" /><div class="quickSearch pull-right" /></div><div class="table" /><div class="footer"><div class="info pull-left" /><div class="page pull-right" /></div>')
+  template : _.template('<div class="header clearfix"><div class="pageSize pull-left" /><div class="quickSearch pull-right" /></div><div class="table" /><div class="footer clearfix"><div class="info pull-left" /><div class="page pull-right" /></div>')
 });
 
 Tableling.Bootstrap.TableView = Tableling.Plain.TableView.extend({});
